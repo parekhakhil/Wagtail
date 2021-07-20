712b1053e77791f7693e8e758f3ff15758e8a0c6
Akhil Parekh <parekhakhil11@gmail.com>
2021-07-20 18:12:28 +0530

    added streams

--- flex/models.py
+++ flex/models.py
@@ -1,9 +1,11 @@
 '''Flexible page'''
 from django.db import models
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel,StreamFieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import StreamField
+from streams import blocks
 
 # Create your models here.
 
@@ -11,13 +13,18 @@ class FlexPage(Page):
     '''Flexible page class'''
     templates = 'flex/flex_page.html'
 
-    # content = StreamFields
+    body = StreamField(
+        [
+            ("title_and_text",blocks.TitleAndTextBlock())
+        ],null=True,
+    )
+
     subtitle = models.CharField(max_length=100,blank=True,null=True)
     image = models.ForeignKey('wagtailimages.Image', null=True, blank=True,related_name='+',on_delete=models.SET_NULL)
     content = RichTextField(features=["bold", 'italic', ], null=True)
 
     content_panels = Page.content_panels+[
-        FieldPanel('subtitle'),FieldPanel('content'),ImageChooserPanel('image')
+        FieldPanel('subtitle'),FieldPanel('content'),ImageChooserPanel('image'),StreamFieldPanel('body')
     ]
 
     class Meta:
