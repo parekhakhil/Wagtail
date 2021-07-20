8197f0d67d02d36dbf2fc1a46d5b4d6c62d2c151
Akhil Parekh <parekhakhil11@gmail.com>
2021-07-20 12:32:38 +0530

    Initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pq=660v$3e5p0p*t1=1d*#2w8r@0=5a^91s+0g#t9+2gd4mlp+'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
